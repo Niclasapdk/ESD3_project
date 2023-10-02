cfd5f354a0b256ade2be0ed3cab6db871e0b8845
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-10-02 21:07:31 +0200

    improve the great graph
    Add
    - Title and labels
    - Larger font and line width
    - Option for log-log plot

--- figures/the_great_graph/the_great_graph.py
+++ figures/the_great_graph/the_great_graph.py
@@ -1,5 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+
+LINEWIDTH = 2
+TITLE = "The Great Graph"
+
+try:
+    if sys.argv[1] == 'log':
+        log = True
+    else:
+        log = False
+except:
+    log = False
 
 # Define constants
 f_clk = 100e6
@@ -17,51 +29,74 @@ def calculate_values(n, c, d_bus):
     return s_p, mem
 
 # Create the figure and subplots
-fig, (ax1_n, ax1_f) = plt.subplots(2, 1)
+fig, (ax1_n, ax1_f) = plt.subplots(2, 1, linewidth=LINEWIDTH)
 
 # Create a twin axes that shares the same x-axis
 ax2_n = ax1_n.twinx()
+plt.title(TITLE)
+plt.grid()
 
 # Create a twin axes for the top axis
 ax2_f = ax1_f.twinx()
+plt.grid()
 
-# Define the range of n values (1 to 100)
-n_values = np.arange(1, 101)
+# Define the range of n values
+n_values = np.arange(1, 81)
 
 # Loop through different categorical variables (e.g., colors)
 for bus_name, bus_speed in busses.items():
-    for c in core_counts:
+    for i, c in enumerate(core_counts):
         # Calculate s_p and mem for each n value
         s_p_values, mem_values = zip(*[calculate_values(n, c, bus_speed) for n in n_values])
 
-        # Plot s_p on the left axis
-        ax1_n.plot(n_values, s_p_values, label=f'{bus_name}, {c} cores - s_p')
-        
-        # Plot mem on the right axis
-        ax2_n.plot(n_values, mem_values, label=f'{bus_name}, {c} cores - mem', linestyle='--')
+        if log:
+            # Plot s_p on the left axis
+            ax1_n.loglog(n_values, s_p_values, label=f'{bus_name}, {c} cores - $s_p$', linewidth=LINEWIDTH)
+            
+            # Plot mem on the right axis
+            ax2_n.loglog(n_values, mem_values, label=f'{bus_name}, {c} cores - mem', linestyle='--', linewidth=LINEWIDTH)
 
-        f_target_values = n_values * c * (f_clk/71)
-        # Plot s_p on the left axis
-        ax1_f.plot(f_target_values, s_p_values, label=f'{bus_name}, {c} cores - s_p')
-        
-        # Plot mem on the right axis
-        ax2_f.plot(f_target_values, mem_values, label=f'{bus_name}, {c} cores - mem', linestyle='--')
+            if i == 1:
+                f_target_values = n_values * c * (f_clk/71)
+                # Plot s_p on the left axis
+                ax1_f.loglog(f_target_values, s_p_values, label=f'{bus_name} - $s_p$', linewidth=LINEWIDTH)
+                
+                # Plot mem on the right axis
+                ax2_f.loglog(f_target_values, mem_values, label=f'{bus_name} - mem', linestyle='--', linewidth=LINEWIDTH)
+        else:
+            # Plot s_p on the left axis
+            ax1_n.plot(n_values, s_p_values, label=f'{bus_name}, {c} cores - $s_p$', linewidth=LINEWIDTH)
+            
+            # Plot mem on the right axis
+            ax2_n.plot(n_values, mem_values, label=f'{bus_name}, {c} cores - mem', linestyle='--', linewidth=LINEWIDTH)
+
+            if i == 1:
+                f_target_values = n_values * c * (f_clk/71)
+                # Plot s_p on the left axis
+                ax1_f.plot(f_target_values, s_p_values, label=f'{bus_name} - $s_p$', linewidth=LINEWIDTH)
+                
+                # Plot mem on the right axis
+                ax2_f.plot(f_target_values, mem_values, label=f'{bus_name} - mem', linestyle='--', linewidth=LINEWIDTH)
 
 # Set labels for the axes
-ax1_n.set_xlabel('n')
-ax1_n.set_ylabel('$s_p$')
-ax2_n.set_ylabel('mem')
-ax1_f.set_xlabel('$f_{target}$')
-ax1_f.set_ylabel('$s_p$')
-ax2_f.set_ylabel('mem')
+ax1_n.set_xlabel('number of nodes, $n$')
+ax1_n.set_ylabel('salts required for maximum utilization\n$s_p$')
+ax2_n.set_ylabel('memory required for hashes struct array\n[Bytes]')
+ax1_f.set_xlabel('Hashing frequency\n$f_{hashing}$ [H/s]')
+ax1_f.set_ylabel('salts required for maximum utilization\n$s_p$')
+ax2_f.set_ylabel('memory required for hashes struct array\n[Bytes]')
 
 # Add legends
 lines, labels = ax1_n.get_legend_handles_labels()
 lines2, labels2 = ax2_n.get_legend_handles_labels()
+lines_f, labels_f = ax1_f.get_legend_handles_labels()
+lines2_f, labels2_f = ax2_f.get_legend_handles_labels()
 lines.extend(lines2)
 labels.extend(labels2)
 ax1_n.legend(lines, labels, loc='upper left')
-ax1_f.legend(lines, labels, loc='upper left')
+lines_f.extend(lines2_f)
+labels_f.extend(labels2_f)
+ax1_f.legend(lines_f, labels_f, loc='upper left')
 
 # Show the plot
 plt.show()

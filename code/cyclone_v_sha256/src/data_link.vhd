2620cf8a373dbba08cd5b8a179abb64140b7fd9b
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-10-19 13:59:14 +0200

    add slave side for layer 2 communication

--- code/cyclone_v_sha256/src/data_link.vhd
+++ code/cyclone_v_sha256/src/data_link.vhd
@@ -5,32 +5,43 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity data_link is
     port(
         -- inputs
-        clk      : in STD_LOGIC; -- system clock
+        --clk      : in STD_LOGIC; -- system clock
         com_clk  : in STD_LOGIC;
-        r_nw     : in STD_LOGIC; -- R/not_W signal (read active high)
-        addr     : in STD_LOGIC_VECTOR(1 downto 0);
+        r_nw     : in STD_LOGIC; -- R/not_W signal (read active high) (as seen by the master)
+        addr_bus : in STD_LOGIC_VECTOR(1 downto 0);
         data_tx  : in STD_LOGIC_VECTOR(7 downto 0);
-        data_rx  : in STD_LOGIC_VECTOR(7 downto 0);
+        -- inout
+        data_bus : inout STD_LOGIC_VECTOR(7 downto 0);
         -- outputs
-        data_out  : out STD_LOGIC_VECTOR(7 downto 0);
-        r_nw_out : out STD_LOGIC; -- mirrored R/now_W signal
+        data_rx  : out STD_LOGIC_VECTOR(7 downto 0);
+
+        -- debug
+        dbg_int_r_nw : out STD_LOGIC
         );
 end data_link;
 
 architecture Behavioral of data_link is
 begin
-    process(clk, com_clk, r_nw)
-        variable recv_data : STD_LOGIC := '1'; -- flag, high when data has not been received this com_clk cycle
+    process(com_clk)
+        variable int_addr : STD_LOGIC_VECTOR(1 downto 0);
+        variable int_r_nw : STD_LOGIC;
     begin
-        if rising_edge(clk) then
-            -- not correct
-            if com_clk = '1' and recv_data = '1' then
-                data_out <= data_rx;
-                recv_data := '0';
-            elsif com_clk = '0' then
-                recv_data := '1';
+        if falling_edge(com_clk) then
+            int_addr := addr_bus;
+            int_r_nw := r_nw;
+            dbg_int_r_nw <= not int_r_nw;
+            if int_addr = "10" then -- we are the ones being talked to
+                if int_r_nw = '1' then -- slave will write to data bus
+                    data_bus <= data_tx; -- setup
+                end if;
             else
-                data_out <= data_out;
+                data_bus <= "ZZZZZZZZ";
+            end if;
+        elsif rising_edge(com_clk) then
+            if int_addr = "10" then -- we are the ones being talked to
+                if int_r_nw = '0' then -- master will have written data bus as last falling edge
+                    data_rx <= data_bus; -- latch
+                end if;
             end if;
         end if;
     end process;

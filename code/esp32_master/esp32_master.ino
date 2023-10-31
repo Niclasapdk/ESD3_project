d545bfacb6d5279b06abb20cb31ffecd277db4d3
Markus Heinrich Toribio <23717111+mhtoribio@users.noreply.github.com>
2023-10-31 13:24:24 +0100

    esp go brrrrr

--- code/esp32_master/esp32_master.ino
+++ code/esp32_master/esp32_master.ino
@@ -31,17 +31,33 @@ const int data_gpios[] = {9, 8, 7, 6, 5, 4, 18, 19};
 dedic_gpio_bundle_handle_t dataDedicHandle = NULL;
 
 SemaphoreHandle_t timerSemaphore;
+QueueHandle_t txQueue, rxQueue;
 
-// Protocol control variables
-uint32_t data;
-uint32_t r_nw;
-uint32_t node_addr;
+#define TX_Q_LEN 100
+#define RX_Q_LEN 100
+
+typedef struct msg {
+    uint8_t data;
+    uint8_t r_nw;
+    uint8_t node_addr;
+} msg_t;
+
+enum BRIDGE_BYTE {
+    BR_START = 'A',
+    BR_STOP = 'B',
+    BR_RD = 'R',
+    BR_WR = 'W'
+};
+
+enum BRIDGE_STATE {
+    BR_S_IDLE, BR_S_RW, BR_S_ADDR, BR_S_DATA, BR_S_STOP
+};
 
 void setupPlusBus() {
     // configure GPIO
     // Create dataDedicHandle, input/output
     gpio_config_t io_conf = {
-        .mode = GPIO_MODE_INPUT_OUTPUT,
+        .mode = GPIO_MODE_INPUT_OUTPUT_OD,
     };
     for (int i = 0; i < sizeof(data_gpios) / sizeof(data_gpios[0]); i++) {
         io_conf.pin_bit_mask = 1ULL << data_gpios[i];
@@ -71,24 +87,130 @@ void clkEventTask(void *pvParameters) {
 
     uint32_t edgeType;
     setupPlusBus();
+
+    msg_t txMsg, rxMsg;
+
+    uint32_t out;
     while (1) {
         if (xSemaphoreTake(timerSemaphore, portMAX_DELAY) == pdTRUE) {
             edgeType = !((REG_READ(GPIO_IN_REG)>>CLK_PIN)&1);
-            if (edgeType) { // Rising Edge
+
+            if (edgeType) {
+                // Rising Edge
                 REG_WRITE(GPIO_OUT_W1TS_REG, 1<<CLK_PIN);
+                if (txMsg.r_nw) {
+                    // Receive data from node
+                    dedic_gpio_bundle_write(dataDedicHandle, 0xff, 0xff); // setup bus for receiving
+                }
+                else {
+                    // Send data to node
+                    dedic_gpio_bundle_write(dataDedicHandle, 0xff, txMsg.data);
+                }
             }
-            else { // Falling Edge
+            else if (!edgeType) {
+                // Falling Edge
                 REG_WRITE(GPIO_OUT_W1TC_REG, 1<<CLK_PIN);
-                //dedic_gpio_bundle_write(dataDedicHandle, 0xff, data);
+
+                if (txMsg.r_nw) {
+                    // Receive data from node
+                    rxMsg.data = dedic_gpio_bundle_read_in(dataDedicHandle);
+                    rxMsg.node_addr = txMsg.node_addr;
+                    xQueueSend(rxQueue, &rxMsg, portMAX_DELAY);
+                }
+
+                xQueueReceive(txQueue, &txMsg, portMAX_DELAY);
+
+                // Setup r_nw, node_addr and clk
+                out = 0 | (txMsg.r_nw<<R_NW_PIN) | ((txMsg.node_addr&0x1)<<ADDR0) | ((txMsg.node_addr>>1)&1);
+                REG_WRITE(GPIO_OUT_W1TS_REG, out);
+                REG_WRITE(GPIO_OUT_W1TC_REG, ~out);
             }
         }
     }
 }
 
+void sendMsg(msg_t *msg) {
+    Serial.print("Node: ");
+    Serial.print(msg->node_addr);
+    Serial.print(",\tData: ");
+    Serial.println(msg->data, HEX);
+}
+
+void bridgeTask(void *pvParameters) {
+    (void)pvParameters; // We don't use the task parameter
+
+    uint8_t c;
+    msg_t rxMsg;
+    msg_t txMsg;
+    enum BRIDGE_STATE state = BR_S_IDLE;
+    Serial.begin(115200);
+    while (1) {
+        if (Serial.available()) {
+            c = Serial.read();
+            switch(state) {
+                case BR_S_IDLE:
+                    if (c == BR_START) {
+                        memset(&txMsg, 0, sizeof(msg_t));
+                        state = BR_S_RW;
+                    }
+                    break;
+                case BR_S_RW:
+                    if (c == BR_RD) {
+                        txMsg.r_nw = 1;
+                        state = BR_S_ADDR;
+                    }
+                    else if (c == BR_WR) {
+                        txMsg.r_nw = 0;
+                        state = BR_S_ADDR;
+                    }
+                    else
+                        state = BR_S_IDLE;
+                    break;
+                case BR_S_ADDR:
+                    if (c > '3' || c < '0') {
+                        state = BR_S_IDLE;
+                    }
+                    else {
+                        txMsg.node_addr = c - '0';
+                        state = BR_S_DATA;
+                    }
+                    break;
+                case BR_S_DATA:
+                    txMsg.data = c;
+                    state = BR_S_STOP;
+                    break;
+                case BR_S_STOP:
+                    if (c == BR_STOP)
+                        xQueueSend(txQueue, &txMsg, portMAX_DELAY);
+                    state = BR_S_IDLE;
+                    break;
+                default:
+                    state = BR_S_IDLE;
+            }
+            Serial.println((int)state);
+        }
+
+        if (xQueueReceive(rxQueue, &rxMsg, 0) == pdTRUE)
+            sendMsg(&rxMsg);
+    }
+}
+
 void setup() {
-    Serial.begin(112500);
     // Create a timer semaphore
     timerSemaphore = xSemaphoreCreateBinary();
+
+    txQueue = xQueueCreate(TX_Q_LEN, sizeof(msg_t));
+    rxQueue = xQueueCreate(RX_Q_LEN, sizeof(msg_t));
+
+    // Create a FreeRTOS task
+    xTaskCreate(
+            bridgeTask,        // Function that implements the task
+            "bridgeTask",      // Name for the task (not required)
+            10000,            // Stack size (words, not bytes)
+            NULL,             // Task parameter (not used)
+            1,                // Priority (0 is idle priority)
+            NULL              // Task handle (not used)
+            );
 
     // Create a FreeRTOS task
     xTaskCreate(
@@ -103,7 +225,7 @@ void setup() {
     // Create a timer to trigger the semaphore every second
     TimerHandle_t timer = xTimerCreate(
             "Timer",                    // Timer name (not required)
-            pdMS_TO_TICKS(1000),         // Timer period in milliseconds
+            pdMS_TO_TICKS(1),         // Timer period in milliseconds
             pdTRUE,                      // Auto-reload timer
             0,                          // Timer ID (not required)
             timerCallback               // Timer callback function
